bbbc0a8f2c990141b448d9adde6b865dc2c7e0f4
nOoBenok <126114744+nOoBenok@users.noreply.github.com>
2025-06-12 15:06:47 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,9 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def browser():
-    # Настройка драйвера
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Для запуска в headless режиме (без GUI)
+    options.add_argument("--headless")  
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     
